28b98e437c4ba37cb08f809ddfa0cc15617d7ce4
feb <dinhhien102@gmail.com>
2023-09-08 20:25:09 +1000

    DEV: mobile update

--- src/component/Cta.js
+++ src/component/Cta.js
@@ -3,8 +3,8 @@ import React from 'react'
 const Cta = () => {
   return (
     <div>
-      <a href="https://www.facebook.com/febstudio.fb" rel="noreferrer" className="button hide--mobile" target="_blank">Get in touch</a>
-      <a href="fb://profile/105337835476355" className="button hide--desktop">Get in touch</a>
+      <a href="https://www.facebook.com/febstudio.fb" rel="noreferrer" className="button hide--mobile" target="_blank">Get in touch <span className='mail-icon'></span></a>
+      <a href="fb://profile/105337835476355" className="button hide--desktop">Get in touch <span className='mail-icon'></span></a>
     </div>
   )
 }

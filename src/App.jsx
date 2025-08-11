7570f583447262759780437238d35ddc1e38b8f6
Feb Dao <dinhhien102@gmail.com>
2025-08-11 23:22:23 +1000

    app: Updated album.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import { useEffect, useState } from 'react'
 import './App.css'
 import Album from './components/Album'
+import PhotoAlbum from './components/PhotoAlbum'
 import data from './data.json'
 
 function App() {
   const { albums } = data
   const [selectedAlbum, setSelectedAlbum] = useState(albums.featured)
   const [toggler, setToggler] = useState(false)
+  const photos = selectedAlbum.images.map((image) => ({
+    src: image,
+    width: 1000,
+    height: 1000,
+  }))
+
+  console.log('photos', photos)
+
   useEffect(() => {
     console.log(selectedAlbum)
   }, [selectedAlbum])
@@ -20,7 +29,8 @@ function App() {
           }}>{albums[album].name}</button>
         ))}
       </div>
-      <Album title={selectedAlbum.name} images={selectedAlbum.images} toggler={toggler} />
+      {/* <Album title={selectedAlbum.name} images={selectedAlbum.images} toggler={toggler} /> */}
+      {photos.length > 0 && <PhotoAlbum photos={photos} />}
     </div>
   )
 }

1c2264b5d23b29269beb6ca230c9876ef9577979
Feb Dao <dinhhien102@gmail.com>
2025-11-19 15:53:41 +1100

    Added albums.

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -1,11 +1,12 @@
 function Album({ album }) {
-  const { photos, name, description, location} = album
+  const { photos, name, description, location, tags } = album
   return (
     <div className="album">
       <div className="album__content mb-5 p-10">
-        <h2 className="album__title block-title text-5xl mb-5">{name}</h2>
+        <h2 className="album__title block-title text-5xl mb-5"><span>{name}</span></h2>
         { description && <p className="album__description text-2xl">{description}</p>}
         { location && <p className="album__location mt-4">⚑ {location}</p>}
+        { tags && <p className="album-cover__tags capitalize mt-4 badge badge-neutral badge-outline">{tags.join(', ')}</p>}
       </div>
       {photos.map((photo) => (
         <div key={photo} className="album__item">

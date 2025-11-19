76919e29898f517cf46732112cc9951428f65f42
Feb Dao <dinhhien102@gmail.com>
2025-11-19 12:23:43 +1100

    Added albums.

--- src/components/Album.jsx
+++ src/components/Album.jsx
@@ -1,10 +1,11 @@
 function Album({ album }) {
-  const { photos, name, description} = album
+  const { photos, name, description, location} = album
   return (
     <div className="album">
       <div className="album__content mb-5 p-10">
         <h2 className="album__title block-title text-5xl mb-5">{name}</h2>
-        <p className="album__description text-2xl">{description}</p>
+        { description && <p className="album__description text-2xl">{description}</p>}
+        { location && <p className="album__location mt-4">⚑ {location}</p>}
       </div>
       {photos.map((photo) => (
         <div key={photo} className="album__item">
--- src/components/AlbumCover.jsx
+++ src/components/AlbumCover.jsx
@@ -1,5 +1,5 @@
 function AlbumCover({ album }) {
-  const { photos, name, description } = album
+  const { photos, name, description, location } = album
   const coverPhoto =
     photos.length > 0
       ? photos[Math.floor(Math.random() * photos.length)]
@@ -7,9 +7,10 @@ function AlbumCover({ album }) {
   return (
     <div className="album-cover">
       <img src={coverPhoto} alt="album-cover" />
-      <div className="album-cover__content">
+      <div className="album-cover__content text-center">
         <h2 className="album-cover__title">{name}</h2>
-        <p className="album-cover__description text-white">{description}</p>
+        <p className="album-cover__description text-white text-xl">{description}</p>
+        { location && <p className="album-cover__location mt-4 text-blue-100 px-1 border border-blue-50">⚑ {location}</p>}
       </div>
     </div>
   )

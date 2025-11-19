0b0754901de4256ec15818fc487f9002f677b619
Feb Dao <dinhhien102@gmail.com>
2025-11-20 10:17:23 +1100

    Updated booking form.

---
+++ src/components/Booking.jsx
@@ -0,0 +1,17 @@
+function Booking() {
+  return (
+    <div className="booking-form">
+      <iframe
+        id="JotFormIFrame-253222382012039"
+        title="Feb Studio Booking Inquiry Form"
+        allowtransparency="true"
+        allow="geolocation; microphone; camera; fullscreen; payment"
+        src="https://form.jotform.com/253222382012039"
+        frameBorder="0"
+      >
+      </iframe>
+    </div>
+  )
+}
+
+export default Booking

90b52375ccf9f2fa2e583a7dcd11d416c530d953
Feb Dao <dinhhien102@gmail.com>
2025-11-20 11:51:52 +1100

    Update spacing.

--- src/App.jsx
+++ src/App.jsx
@@ -69,10 +69,10 @@ function App() {
     <div data-theme="corporate">
       <div className={`main-content ${toggler ? 'is-inactive' : ''}`}>
         <Hero photos={heroPhotos} />
-        <div className="content bg-amber-50 content p-10 md:p-20">
+        <div className="content bg-amber-50 content px-3 py-10 md:px-5 md:py-20">
           <div className="container mx-auto">
-            <h2 className="text-3xl md:text-6xl font-bold text-center mb-10 block-title">Our <span>{formatPhotoTypeLabel(selectedType)}</span> Projects</h2>
-            <div className="project-filter flex justify-center mb-10">
+            <h2 className="text-3xl md:text-6xl font-bold text-center mb-4 md:mb-10 block-title">Our <span>{formatPhotoTypeLabel(selectedType)}</span> Projects</h2>
+            <div className="project-filter flex justify-center mb-5 md:mb-10">
               <div className="filter">
                 {photoTypes.map((type) => (
                   <input

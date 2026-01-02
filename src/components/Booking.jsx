9837ce03cac0c1b5aa05a4faff0c41ab91a162ec
Feb Dao <dinhhien102@gmail.com>
2026-01-02 12:11:37 +1100

    Created header.

--- src/components/Booking.jsx
+++ src/components/Booking.jsx
@@ -1,15 +1,20 @@
+import Header from "./Header"
+
 function Booking() {
   return (
-    <div className="booking-form">
-      <iframe
-        id="JotFormIFrame-253222382012039"
-        title="Feb Studio Booking Inquiry Form"
-        allowtransparency="true"
-        allow="geolocation; microphone; camera; fullscreen; payment"
-        src="https://form.jotform.com/253222382012039"
-        frameBorder="0"
-      >
-      </iframe>
+    <div>
+      <Header />
+      <div className="booking-form">
+        <iframe
+          id="JotFormIFrame-253222382012039"
+          title="Feb Studio Booking Inquiry Form"
+          allowtransparency="true"
+          allow="geolocation; microphone; camera; fullscreen; payment"
+          src="https://form.jotform.com/253222382012039"
+          frameBorder="0"
+        >
+        </iframe>
+      </div>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,23 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter } from 'react-router'
+import { RouterProvider } from 'react-router'
 import App from './App.jsx'
+import Booking from './components/Booking.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/booking',
+    element: <Booking />,
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )

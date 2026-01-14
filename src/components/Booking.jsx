94cfc949999f216707e584cc2548dd4b0136f402
Feb Dao <dinhhien102@gmail.com>
2026-01-14 12:09:35 +1100

    Optimize css and js.

--- src/components/Booking.jsx
+++ src/components/Booking.jsx
@@ -1,4 +1,47 @@
+import { useEffect } from 'react'
+
+const JOTFORM_SCRIPT_SRC =
+  'https://cdn.jotfor.ms/s/umd/latest/for-form-embed-handler.js'
+const JOTFORM_IFRAME_SELECTOR = "iframe[id='JotFormIFrame-253222382012039']"
+const JOTFORM_DOMAIN = 'https://form.jotform.com/'
+
 function Booking() {
+  useEffect(() => {
+    if (typeof window === 'undefined') return
+
+    const initHandler = () => {
+      if (typeof window.jotformEmbedHandler === 'function') {
+        window.jotformEmbedHandler(JOTFORM_IFRAME_SELECTOR, JOTFORM_DOMAIN)
+      }
+    }
+
+    let script = document.querySelector(
+      `script[src="${JOTFORM_SCRIPT_SRC}"]`,
+    )
+
+    if (script) {
+      if (typeof window.jotformEmbedHandler === 'function') {
+        initHandler()
+        return
+      }
+
+      script.addEventListener('load', initHandler)
+      return () => {
+        script.removeEventListener('load', initHandler)
+      }
+    }
+
+    script = document.createElement('script')
+    script.src = JOTFORM_SCRIPT_SRC
+    script.defer = true
+    script.addEventListener('load', initHandler)
+    document.body.appendChild(script)
+
+    return () => {
+      script.removeEventListener('load', initHandler)
+    }
+  }, [])
+
   return (
     <div>
       <div className="booking-form">
@@ -10,6 +53,7 @@ function Booking() {
             allow="geolocation; microphone; camera; fullscreen; payment"
             src="https://form.jotform.com/253222382012039"
             frameBorder="0"
+            loading="lazy"
           >
           </iframe>
         </div>
